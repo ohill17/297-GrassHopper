31f965bd795d3371926121e9f617e47e4f88131e
KelsieMcDonald-2003 <kelsie.lin.mcdonald@gmail.com>
2024-03-14 14:26:28 -0700

    KM-Code1
    This is Kelsie's code for the scaffolding part of the code.

--- GrassHopper/GrassHopper/Views/Shared/Error.cshtml.cs
+++ GrassHopper/GrassHopper/Views/Shared/Error.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace GrassHopper.Pages
+namespace GrassHopper.Views.Shared
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
