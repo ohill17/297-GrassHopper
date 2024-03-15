5d77f5a6ca8c1b9ffc711e086d393ea711998546
KelsieMcDonald-2003 <kelsie.lin.mcdonald@gmail.com>
2024-03-15 14:31:27 -0700

    I got the Contact Us page going. Same thing, not much going on with that page.

---
+++ GH/GH/Controllers/ContactsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GH.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
