b2ad8b1caefa2a3cef7f674ffc576461cc5a4207
KelsieMcDonald-2003 <kelsie.lin.mcdonald@gmail.com>
2024-04-06 11:03:54 -0700

    I debugged the errors that Charlese saw. There were a couple of typos in the code. It should now be good as new.

--- GrassHopper/GrassHopper/Data/Repositories/IPhotosRepository.cs
+++ GrassHopper/GrassHopper/Data/Repositories/IPhotosRepository.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using GrassHopper.Data;
+using GrassHopper.Models;
+
+namespace GrassHopper.Data.Repositories
+{
+    public interface IPhotosRepository
+    {
+
+    }
+
+    public class PhotosRepository : IPhotosRepository
+    {
+
+    }
+
+    public class FakePhotosRepository : IPhotosRepository
+    {
+
+    }
+}
