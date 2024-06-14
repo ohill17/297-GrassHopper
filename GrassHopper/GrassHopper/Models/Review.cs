41c9f87afd8f5d18633f3f3581a26b97f2c7f7d1
Orion Hill <Orion123333@gmail.com>
2024-06-14 13:20:26 -0700

    Minor Bug fixes and Model Validation

--- GrassHopper/GrassHopper/Models/Review.cs
+++ GrassHopper/GrassHopper/Models/Review.cs
@@ -1,15 +1,26 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace GrassHopper.Models
 {
     public class Review
     {
         [Key]
-       public int ReviewID { get; set; }
-       public string ReviewerName { get; set; }
-       public string ReviewBody { get; set; }
-       public int ReviewRating {  get; set; }
-       public DateTime ReviewDate { get; set; }
-    
+        public int ReviewID { get; set; }
+
+        [Required(ErrorMessage = "Reviewer name is required.")]
+        [StringLength(50, ErrorMessage = "Reviewer name can't be longer than 50 characters.")]
+        public string ReviewerName { get; set; }
+
+        [Required(ErrorMessage = "Review body is required.")]
+        [StringLength(528, ErrorMessage = "Review body can't be longer than 528 characters.")]
+        public string ReviewBody { get; set; }
+
+        [Required(ErrorMessage = "Review rating is required.")]
+        [Range(1, 5, ErrorMessage = "Must Select Between 1-5 stars")]
+        public int ReviewRating { get; set; }
+
+        [Required]
+        public DateTime ReviewDate { get; set; }
     }
 }
